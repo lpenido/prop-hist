3ecbf779fab02d8acd028a9d19e91e11c40392d3
Lucas Penido <lucaspenido@Lucass-MacBook-Air.local>
2021-09-19 23:41:18 -0500

    added db and popup support

---
+++ eda.py
@@ -0,0 +1,22 @@
+
+def count_sale_refinance(pin):
+    """
+    """
+    pass
+
+
+def get_current_owner(pin):
+    """
+    Returns the name of the first grantee on the most 
+    recent mortgage of a given PIN
+
+    :pin: str
+    :current_owner: str
+    """
+    return current_owner
+
+def is_current_owner_an_llc(pin):
+    """
+    """
+    current_owner = get_current_owner(pin)
+    
--- scrape.py
+++ scrape.py
@@ -1,7 +1,11 @@
 import os
 import csv
 import time
-import logging 
+import logging
+import sqlite3
+import random
+
+import pandas as pd
 
 # Environment
 from dotenv import load_dotenv
@@ -9,7 +13,7 @@ load_dotenv()
 
 # General utility 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
@@ -17,11 +21,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 
 # HTML Parsing
 from bs4 import BeautifulSoup
 
+# Config vars
 url = "http://162.217.184.82/i2/default.aspx?AspxAutoDetectCookieSupport=1" # url to hit
 s_delay, m_delay, l_delay = 3, 5, 10 # delays to be used
 
@@ -33,12 +38,16 @@ result_page = {
 home_page = {
     "home_btn": '//*[@id="Navigator1_SearchHome1"]',
     "search_btn": '//*[@id="SearchFormEx1_btnSearch"]',
+    "reset": '//*[@id="SearchFormEx1_BtnReset"]',
     "s0": '//*[@id="SearchFormEx1_PINTextBox0"]', 
     "s1": '//*[@id="SearchFormEx1_PINTextBox1"]',
     "s2": '//*[@id="SearchFormEx1_PINTextBox2"]',
     "s3": '//*[@id="SearchFormEx1_PINTextBox3"]',
-    "s4": '//*[@id="SearchFormEx1_PINTextBox4"]'
+    "s4": '//*[@id="SearchFormEx1_PINTextBox4"]',
 }
+
+def rand_rest():
+    time.sleep(random.randint(0, 3))
 
 def is_page_valid(driver, page_info):
     """
@@ -53,10 +62,16 @@ def is_page_valid(driver, page_info):
         element = page_info[key]
         try:
             que = EC.presence_of_element_located((By.XPATH, element))
-            WebDriverWait(driver, m_delay).until(que)
+            WebDriverWait(driver, l_delay).until(que)
+        
+        except TimeoutException:
+            logger.warning(f"Page timed out on {key}")
+            return False
+
         except NoSuchElementException:
             logger.warning(f'{key} NOT found on page')
             return False
+
     logger.info("Page is valid")
     return True
 
@@ -64,6 +79,17 @@ def smart_action(driver, x_path, delay):
     """Wrapper function to wait for the element to appear in the DOM"""
     que = EC.presence_of_element_located((By.XPATH, x_path))
     element = WebDriverWait(driver, delay).until(que)
+    return element
+
+def zero_hits(driver):
+    """Helper function to deal with 0 hits pop up window"""
+    zero_hits = '//*[@id="MessageBoxCtrl1_buttonmbatCLIENTOK"]'
+    element = smart_action(driver, zero_hits, l_delay)
+    element.click()
+
+def wait_until_unblocked(driver):
+    que = EC.invisibility_of_element_located((By.ID, 'MessageBoxCtrl1_ScreenBlocker'))
+    element = WebDriverWait(driver, l_delay).until(que)
     return element
 
 def search_pin(driver, pin):
@@ -82,17 +108,41 @@ def search_pin(driver, pin):
     )
     split_pin = pin.split("-")
     zipped_list = zip(split_pin, search_box)
+
     for pin_part, search_box in zipped_list:
         try:
             element = smart_action(driver, search_box, m_delay)
             element.clear() # some reason this raised stale element errors
             element.send_keys(pin_part)
+
+        # If it's state, try redefining it and running 
+        except StaleElementReferenceException:
+            text_box = smart_action(driver, search_box, l_delay)
+            text_box.send_keys(pin_part)
+
         except NoSuchElementException:
             logger.warning(f'{pin_part} failed to search')
-            return False
+
+    element = smart_action(driver, home_page["search_btn"], l_delay)
+    rand_rest()
+    try:
+        element.click()
     
-    element = smart_action(driver, home_page["search_btn"], s_delay)
-    element.click()
+    # If it's state, try redefining it and running 
+    except StaleElementReferenceException:
+        button = smart_action(driver, home_page["search_btn"], l_delay)
+        button.click()
+
+    # If there's a pop-up, log it and freeze to take a look
+    except ElementClickInterceptedException:
+        try:
+            wait_until_unblocked(driver)
+            zero_hits(driver)
+
+        except Exception as e:
+            print(e)
+            breakpoint()
+    
 
 def is_next_button_present(driver):
     """
@@ -184,15 +234,21 @@ def is_duplicate(conn, record):
         record["first_grantee"],
         record["first_prior_doc_num"]
     )
+
+    # Connection should still be open since it's inside main()
     cur = conn.cursor()
     searched_record = cur.execute(sql, record_to_search)
-    if searched_record != None:
+    searched_record = searched_record.fetchone()
+
+    if searched_record == None:
         return False
     else:
         return True
 
 def insert_record_to_db(conn, record):
     """
+    To insert a singular row into the db
+
     """
     if is_duplicate(conn, record):
         logger.info(f"Duplicated Record: {record}")
@@ -217,27 +273,39 @@ def insert_record_to_db(conn, record):
         cur.execute(sql, record_to_add)
         conn.commit()
 
-def save_scrape_to_csv(results):
+def save_scrape_to_db(conn, results):
+    """
+    To add a list of results to the db
+
+    :results: list of dicts
+    """
+    for result in results:
+        insert_record_to_db(conn, result)
+
+def save_scrape_to_csv(results, output_filename="results.csv"):
     """
     Saves the results to a hardcoded outfile for now. Uses the 'a' flag
     for 'append' instead of 'w' for 'writing' to not overwrite other pins
 
-    TODO: Add a sqlite3 db if the prelim data has legs
+    Might contain duplicates. We'll fix it in post...
 
     :results: list of dicts
     """
-    output_filename = "results.csv"
+    fieldnames = ["recorded_date", "pin", "type_desc", "doc_num", "first_grantor", "first_grantee", "first_prior_doc_num"]
     with open(output_filename, 'a') as f:
-        dict_writer = csv.DictWriter(f, fieldnames=results[0].keys())
-        dict_writer.writeheader()
+        dict_writer = csv.DictWriter(f, fieldnames=fieldnames)
         dict_writer.writerows(results)
 
 def return_to_home_page(driver):
     """Helper function to restart the loop for the next search"""
     home_btn = home_page["home_btn"]
-    driver.find_element_by_xpath(home_btn).click()
+    try:
+        driver.find_element_by_xpath(home_btn).click()
+    except ElementClickInterceptedException:
+        wait_until_unblocked(driver)
+        driver.find_element_by_xpath(home_btn).click()
 
-def search_and_save_pin(driver, pin):
+def search_and_save_pin(driver, pin, conn):
     """
     Searches the pin, if there are multiple pages, it will scrape until no
     next button is found, then save to csv and return to the home page to 
@@ -245,31 +313,38 @@ def search_and_save_pin(driver, pin):
 
     :driver: selenium webdriver object
     :pin: string, unique parcel IDs to search records for
+    :conn: sqlite3 connection object
     """
     try:
         assert is_page_valid(driver, home_page) == True
 
-        logger.info(f"Searching: {pin}")
+        logger.info(f"{pin}: Searching")
         search_pin(driver, pin)
-        logger.info(f"Searched: {pin}")
+        logger.info(f"{pin}: Searched")
 
         # The case of n next buttons
         results = []
-        logger.info(f"Scraping: {pin}")
+        logger.info(f"{pin}: Scraping")
         if is_page_valid(driver, result_page) and is_next_button_present(driver):
             while is_next_button_present(driver):
                 result = scrape_result_table(driver)
                 results += result
                 go_to_next_page(driver)
-                time.sleep(m_delay)
+                rand_rest()
 
         # The case of no next buttons/last page 
         if is_page_valid(driver, result_page) and not is_next_button_present(driver):
             result = scrape_result_table(driver)
             results += result
         
-        logger.info(f"Scraped: {pin}")
+        logger.info(f"{pin}: Scraped")
+        
+        # Gotta get the db and the hard copy
+        logger.info(f"{pin}: Saving")
+        save_scrape_to_db(conn, results)
         save_scrape_to_csv(results)
+        logger.info(f"{pin}: Saved")
+
         return_to_home_page(driver)
     
     except AssertionError:
@@ -282,30 +357,40 @@ def main(pins):
 
     :pins: list of strings, of unique parcel IDs to search records for
     """
+    # Quick type check
+    assert isinstance(pins, list) == True
+
     # Selenium init
     logger.info("Starting driver.")
     options = Options()
     # options.add_argument("--headless") # Comment out to see the browser
+
     driver = webdriver.Firefox(options=options)
 
+    logger.info("Opening connection.")
+    conn = sqlite3.connect(db_file)
+
     try:
+
+        rand_rest()
+
         driver.get(url)
 
-        time.sleep(m_delay)
-
         for pin in pins:
-            logger.info(f"Starting: {pin}")
 
-            # General except in case of breakage, to keep on keeping on
+            logger.info(f"{pin}: Starting")
+
+            # General try/except in case of breakage, to keep on keeping on
             try:
-                search_and_save_pin(driver, pin)
-                logger.info(f"Finished: {pin}")
+                search_and_save_pin(driver, pin, conn)
+                logger.info(f"{pin}: Finished")
+
             except:
                 logging.exception(f"Error occured on {pin}")
                 logging.info("Moving on...")
                 pass 
                 
-        time.sleep(m_delay)
+        rand_rest()
 
     finally:
         logger.info("Closing driver...")
@@ -313,36 +398,62 @@ def main(pins):
         driver.quit()
         logger.info("Drivers Closed.")
 
+        logger.info("Closing connection...")
+        conn.close()
+        logger.info("Connection Closed.")
+
 if __name__ == "__main__":
+
+    # Make sure csv is available to append
+    if not os.path.exists("results.csv"):
+        output_filename = "results.csv"
+        fieldnames = ["recorded_date", "pin", "type_desc", "doc_num", "first_grantor", "first_grantee", "first_prior_doc_num"]
+        with open(output_filename, 'w') as f:
+            dict_writer = csv.DictWriter(f, fieldnames=fieldnames)
+            dict_writer.writeheader()
+
+    # Make sure db exits
+    db_file = os.environ.get("DB")
+    if not os.path.exists(db_file):
+        sql = """
+        CREATE TABLE IF NOT EXISTS records (
+            id integer PRIMARY KEY,
+            recorded_date text NOT NULL,
+            pin text,
+            type_desc text NOT NULL,
+            doc_num text NOT NULL,
+            first_grantor text NOT NULL,
+            first_grantee text NOT NULL,
+            first_prior_doc_num text NOT NULL
+        );"""
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute(sql)
+        conn.commit()
+        conn.close()
 
     # Make sure logs will log
     if not os.path.exists('logs'):
         os.mkdir('logs')
  
+    # File logging
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
     file_handler = logging.FileHandler('./logs/scraper.log')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-    # Stream
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    chformatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(chformatter)
-    logger.addHandler(ch)
+    # Stream logging 
+    streamer = logging.StreamHandler()
+    streamer.setLevel(logging.DEBUG)
+    streamer.setFormatter(formatter)
+    logger.addHandler(streamer)
 
-    test_pin = [
-        "17-34-326-006-0000",
-        "17-27-402-021-0000",
-        "17-34-312-093-1008",
-        "17-34-102-051-1078"
-    ]
+    pins = pd.read_csv("douglas_pins.csv")["pin"].to_list()
+    test_pins = pins[:50] 
 
     start = time.perf_counter()
-    main(test_pin)
+    main(test_pins)
     end = time.perf_counter()
-    print(f"Scraped {len(test_pin)} PIN(s) in {end:0.4f} seconds") # To get an estimate for how long a whole nbhd would take
+    print(f"Scraped {len(test_pins)} PIN(s) in {end:0.4f} seconds") # To get an estimate for how long a whole nbhd would take
